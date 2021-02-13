a958d6330e526aca1765dad370de19bc00b7a6e9
Paul Balitema Kabali <engineerbpk@gmail.com>
2021-02-13 03:11:23 +0300

    Create empty react app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Welcome to our bookstore</header>
+    </div>
+  );
+}
+
+export default App;
