867a0ef4565d3acb606b9579f4654c9a8859568c
Paul Balitema Kabali <engineerbpk@gmail.com>
2021-02-13 15:48:02 +0300

    Implement books reducer and actions

---
+++ src/reducers/books.js
@@ -0,0 +1,16 @@
+import { CREATE_BOOK, REMOVE_BOOK } from '../actions/actionTypes';
+
+const initialState = [];
+
+const bookReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CREATE_BOOK:
+      return [...state, action.payload];
+    case REMOVE_BOOK:
+      return state.filter((book) => book.id !== action.payload.id);
+    default:
+      return state;
+  }
+};
+
+export default bookReducer;
